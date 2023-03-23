a1efac6a10381aab41d25ad3821bfdf01295ac55
Germán Matheus <germanmatheus.dev@gmail.com>
2023-03-22 21:16:57 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Tarea2 2docorte
+Tarea del 2do corte para la materia de JavaScript
---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+Escriba un programa que calcule el sueldo quincenal de un empleado, tomando en cuenta sus datos personales
+(nombre, apellido y cédula) donde el costo de la hora diurna es de 675 Bs, la hora vespertina es de 700 Bs,
+y la hora nocturna es de 956,23 Bs. Si el empleado devenga menos de 85000, tendrá un descuento de ahorro habitacional
+de 0,1% y de seguro social de 0,15%. Si el empleado devenga entre 85.000 y 150.000, tendrá un descuento de ahorro habitacional
+de 0,15% y de seguro social de 0,2% Por último, si el empleado devenga más de 150000, tedrá un descuento de ahorro habitacional de
+0,3% y de seguro social de 0,25%
+
+Visualizar sus datos personales, sueldo quincenal sin deducción, descuento de ahorro habitacional, seguro social y sueldo quincenal
+con deducción (los descuentos pautados en las deducciones son mensuales)
+
+Se asumen como ciertos los siguientes postulados:
+
+**El empleado trabaja 40 horas semanales**
+**El empleado trabaja 8 horas al día**
+*/
+let falseNess = false
+
+function EnviarDatosInicial(e){
+    e.preventDefault()
+}
+
